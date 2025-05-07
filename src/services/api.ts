b67bc554e5205cb2a6c6cc29c37fc4171c0744c0
Prakhar077 <prakharsharma522@gmail.com>
2025-05-07 05:13:10 -0700

    Commit 2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000', // ✅ Use full backend URL
+  baseURL: import.meta.env.VITE_API_URL, // ✅ Use full backend URL
 });
 
 
